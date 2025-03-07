6b5a7518e8481b8a7f83f6d38857e9b614ca05ca
GOVIND AVC <eemail2govindavc@gmail.com>
2025-03-07 10:46:02 +0530

    added changes

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PostCard from './components/postcard'
+
+function App() {
+
+  const posts = [
+    {
+      id: 1,
+      profileImage: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg',
+      username: 'bob',
+      content: 'This is my first post!',
+      isLiked: false,
+    },
+    {
+      id: 2,
+      profileImage: 'https://writestylesonline.com/wp-content/uploads/2016/08/Follow-These-Steps-for-a-Flawless-Professional-Profile-Picture.jpg',
+      username: 'alice',
+      content: 'Loving this social media app.',
+      isLiked: true,
+    },
+    {
+      id: 3,
+      profileImage: 'https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png',
+      username: 'charlie',
+      content: 'Just another day...',
+      isLiked: false,
+    },
+  ];
+
+  return (
+    <>
+    <div className='min-h-screen bg-gray-100 p-6'>
+    <h1 className="text-2xl font-bold mb-4">Social Feed</h1>
+        <div className="space-y-4">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+        </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
